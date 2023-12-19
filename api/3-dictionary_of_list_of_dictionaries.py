4d6c9b3f164aca372c6c9e5226cf5455b0dff83f
ucflumm <ucflumm@gmail.com>
2023-12-19 12:43:53 +1100

    Refactor code to fetch user information from API and use a dictionary for username lookup

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -7,6 +7,10 @@ import requests
 import sys
 
 
+import requests
+import sys
+import json
+
 api_url = "https://jsonplaceholder.typicode.com"
 
 all_employee_tasks = {}
@@ -19,20 +23,25 @@ if todos_response.status_code != 200:
 
 todos_data = todos_response.json()
 
+# Fetch user information for all users first
+users_response = requests.get(f'{api_url}/users')
+if users_response.status_code != 200:
+    print('Failed to fetch user data from the API.')
+    sys.exit(1)
+
+users_data = users_response.json()
+user_dict = {user['id']: user['username'] for user in users_data}
+
 for todo in todos_data:
     user_id = todo['userId']
     task_info = {
-        "username": None,
+        "username": user_dict.get(user_id, None),  # Use user_dict to get the username
         "task": todo['title'],
         "completed": todo['completed']
     }
 
     if user_id not in all_employee_tasks:
-        user_response = requests.get(f'{api_url}/users/{user_id}')
-        if user_response.status_code == 200:
-            user_data = user_response.json()
-            task_info["username"] = user_data['username']
-            all_employee_tasks[user_id] = []
+        all_employee_tasks[user_id] = []
 
     all_employee_tasks[user_id].append(task_info)
 

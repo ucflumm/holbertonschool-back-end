6a93bd2317f9cec189bb86528c8fa0dc6e95312c
ucflumm <ucflumm@gmail.com>
2023-12-19 12:38:00 +1100

    Refactor API script to fetch and organize employee tasks

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
 """
     Returns information about an employee's TODO list progress.
-    Requirements:
-        Records all tasks that are owned by this employee
-            Format: { "USER_ID": [
-                {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
-                {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ],
-                "USER_ID": [
-                {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
-                {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]
-            }
 """
 import json
 import requests
 import sys
 
 
-if __name__ == '__main__':
-    # Base URL for the REST API
-    api_url = "https://jsonplaceholder.typicode.com"
-    employee_id = int(sys.argv[1])
+import requests
+import sys
+import json
 
-    user_response = requests.get(f'{api_url}/users/{employee_id}')
-    todos_response = requests.get(f'{api_url}/todos?userId={employee_id}')
+api_url = "https://jsonplaceholder.typicode.com"
 
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print('Failed to fetch data from the API.')
-        sys.exit(1)
+all_employee_tasks = {}
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+todos_response = requests.get(f'{api_url}/todos')
 
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump({employee_id: [{
-            'task': todo['title'],
-            'completed': todo['completed'],
-            'username': user_data['username']
-        } for todo in todos_data]}, f)
+if todos_response.status_code != 200:
+    print('Failed to fetch data from the API.')
+    sys.exit(1)
+
+todos_data = todos_response.json()
+
+for todo in todos_data:
+    user_id = todo['userId']
+    task_info = {
+        "username": None,
+        "task": todo['title'],
+        "completed": todo['completed']
+    }
+
+    if user_id not in all_employee_tasks:
+        user_response = requests.get(f'{api_url}/users/{user_id}')
+        if user_response.status_code == 200:
+            user_data = user_response.json()
+            task_info["username"] = user_data['username']
+            all_employee_tasks[user_id] = []
+
+    all_employee_tasks[user_id].append(task_info)
+
+with open('todo_all_employees.json', 'w') as json_file:
+    json.dump(all_employee_tasks, json_file, indent=4)

13df3ab67bcef06971e06adddc262bac94891b1a
ucflumm <ucflumm@gmail.com>
2023-12-19 11:56:54 +1100

    Add script to gather data from an API

---
+++ 0-gather_data_from_an_APi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Write a python script using the REST API
+"""
+import requests
+import sys
+
+api_url = 'https://jsonplaceholder.typicode.com'
+employee_id = int(sys.argv[1])
+
+user_response = requests.get(f'{api_url}/users/{employee_id}')
+todos_response = requests.get(f'{api_url}/todos?userId={employee_id}')
+
+if user_response.status_code != 200 or todos_response.status_code != 200:
+    print('Failed to fetch data from the API.')
+    sys.exit(1)
+
+user_data = user_response.json()
+todos_data = todos_response.json()
+
+total_tasks = len(todos_data)
+completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+print(f"Employee {user_data['name']} is done with tasks"
+      f"({completed_tasks}/{total_tasks}):")
+
+for todo in todos_data:
+    if todo['completed']:
+        print(f"\t{todo['title']}")
